4d5b09ceeba9b7cdf1eb57eeff765e1a0eff1596
indramsyafutra <indra.mulia.syafutra@uin-suska.ac.id>
2025-10-29 21:52:20 +0700

    app.js path to node.path

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const mysql = require('mysql');
 const app = express()
-const path = require('path')
+const path = require('node:path')
 require('dotenv').config();
 
 app.disable('x-powered-by');
